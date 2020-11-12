c3f77e7a0a77beababd8dacaaef380393131f2e5
LuisLCampos <llcampos24@gmail.com>
2020-11-12 18:19:43 -0300

    cambio de navbar brand

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+ $(document).ready(function(){
+    $("a").click(function(event){
+        if (this.hash !== "") {
+          event.preventDefault();
+    
+          var gato = this.hash;
+    
+          $("html, body").animate({
+            scrollTop: $(gato).offset().top
+          }, 800, function(){
+            window.location.hash = gato;
+          });
+        }
+      });
+    $('.carousel').carousel();
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+  
+})
+    
